d2877414e386fe7eababcd661052898043532e8f
Juan Antonio Cuesta Tejera <juancuesta1987@gmail.com>
2025-01-28 23:31:46 +0100

    Agrega funciones y comentarios PHPDoc a pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -1,31 +1,71 @@
 <?php
-if (isset($_GET['pokemon'])) {
-    $pokemon = strtolower(trim($_GET['pokemon']));
+/**
+ * Obtiene los datos de un Pokémon desde la PokeAPI.
+ *
+ * @param string $pokemon Nombre o ID del Pokémon a buscar.
+ * @return array|null Arreglo con los datos del Pokémon o null si no se encuentra.
+ */
+function getPokemonData($pokemon) {
     $url = 'https://pokeapi.co/api/v2/pokemon/' . urlencode($pokemon);
 
     // Obtener datos de la API usando file_get_contents()
     $response = @file_get_contents($url);
 
     if ($response === FALSE) {
-        $error = "No se encontró el Pokémon '$pokemon'. Por favor, intenta de nuevo.";
+        return null;
     } else {
         $data = json_decode($response, true);
+        return $data;
+    }
+}
 
-        // Extraer información relevante
-        $name = ucfirst($data['name']);
-        $image = $data['sprites']['front_default'];
-        $height = $data['height'] / 10; 
-        $weight = $data['weight'] / 10; 
+/**
+ * Extrae información relevante de los datos del Pokémon.
+ *
+ * @param array $data Arreglo asociativo con los datos del Pokémon.
+ * @return array Arreglo con la información procesada del Pokémon.
+ */
+function extractPokemonInfo($data) {
+    // Extraer información relevante
+    $info = [];
 
-        $abilities = [];
-        foreach ($data['abilities'] as $ability) {
-            $abilities[] = $ability['ability']['name'];
-        }
+    $info['name'] = ucfirst($data['name']);
+    $info['image'] = $data['sprites']['front_default'];
+    $info['height'] = $data['height'] / 10; // Convertir decímetros a metros
+    $info['weight'] = $data['weight'] / 10; // Convertir hectogramos a kilogramos
 
-        $types = [];
-        foreach ($data['types'] as $type) {
-            $types[] = $type['type']['name'];
-        }
+    $info['abilities'] = [];
+    foreach ($data['abilities'] as $ability) {
+        $info['abilities'][] = $ability['ability']['name'];
+    }
+
+    $info['types'] = [];
+    foreach ($data['types'] as $type) {
+        $info['types'][] = $type['type']['name'];
+    }
+
+    return $info;
+}
+
+if (isset($_GET['pokemon'])) {
+    $pokemon = strtolower(trim($_GET['pokemon']));
+
+    // Obtener los datos del Pokémon usando la función
+    $data = getPokemonData($pokemon);
+
+    if ($data === null) {
+        $error = "No se encontró el Pokémon '$pokemon'. Por favor, intenta de nuevo.";
+    } else {
+        // Extraer la información relevante usando la función
+        $info = extractPokemonInfo($data);
+
+        // Desestructurar el arreglo $info para facilitar su uso en el HTML
+        $name = $info['name'];
+        $image = $info['image'];
+        $height = $info['height'];
+        $weight = $info['weight'];
+        $abilities = $info['abilities'];
+        $types = $info['types'];
     }
 } else {
     // Redirigir al inicio si no se recibe ningún parámetro
